214fea61e47e4bd2272e73347798409f8864112f
Mel <melissapixell@gmail.com>
2025-08-30 19:12:56 +0300

    setting img

--- header.php
+++ header.php
@@ -1,11 +1,18 @@
 <div class="header">
-    <div class="header-left">
-        <h2>
-            Добро пожаловать!
-        </h2>
+    <div class="header-header">
+        <div class="header-left">
+            <h2>
+                Добро пожаловать!
+            </h2>
+        </div>
+        
+        <div class="header-right">
+            <img src="img/add/meteor-rain.gif" alt="img">
+        </div>
+    </div>
+
+    <div class="header-option">
+        <img src="img/add/options.gif" alt="img">
     </div>
     
-    <div class="header-right">
-        <img src="img/add/meteor-rain.gif" alt="img">
-    </div>
 </div>
